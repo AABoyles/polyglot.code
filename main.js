7ad2093bef5f78f75395b2b2e8ac3b74b636746a
Tony Boyles <aaboyles@gmail.com>
2015-08-28 20:34:49 +0000

    First prototype. Lots of glitches, but illustrates the idea of a simple Javascript REPL

---
+++ main.js
@@ -0,0 +1,39 @@
+var scratchPads = [];
+
+$(function(){
+  scratchPads.push(CodeMirror($("#scratch")[0]));
+  
+  
+  console.log = function(thing){
+    $("#run").append("<span class='input'>" + thing.toString().split(/\n/).join("<br />")+"</span><br />");
+  };
+  
+  execute = function(code){
+    console.log(code);
+    var output = "";
+    if($("#language").val()=="Javascript"){
+      output = eval(code);
+    }
+    $("#run").append("<span class='output'>" + output + "</span><br />");
+  };
+  
+  $("#current").keydown(function(e){
+    if(e.keyCode == 13){
+      execute($(this).text());
+      $(this).text("");
+    }
+  });
+  
+  $("#execute button").click(function(e){
+    var code = "";
+    if(this.innerTHML == "Selection"){
+      code = scratchPads[0].getSelection();
+    } else if (this.innerHTML == "Line") {
+      code = scratchPads[0].getLine(scratchPads[0].getCursor().line);
+    } else {
+      code = scratchPads[0].getValue();
+    }
+    execute(code);
+  })
+  
+});
